664db8c993803b32f38170699ae3a0c0d9d6832b
jatul <atul.jaiswar@example.com>
2025-03-17 02:03:40 +0530

    Added we believe section

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ crossIcon.addEventListener("click", () => {
   menuList.classList.remove("showMenuList");
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".believe-swiper", {
+      slidesPerView: 1.2, // Center full + 10% side visibility
+      centeredSlides: true,
+      loop: true,
+      spaceBetween: 15,
+      breakpoints: {
+        768: {
+          slidesPerView: 2, // Show more slides on larger screens
+          spaceBetween: 20,
+          centeredSlides: false,
+        },
+      },
+      pagination: {
+          el: ".swiper-pagination", // This will now reference our outside pagination
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '"></span>';
+          },
+        },
+    });
+  });
